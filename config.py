5f316c5014b28b658db6798453f89cb6150aaaa6
Qwerty59a <116958040+Qwerty59a@users.noreply.github.com>
2022-12-12 17:07:59 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+API_ID: int = int(os.getenv("API_ID"))
+API_HASH: str = os.getenv("API_HASH")
+BOT_TOKEN: str = os.getenv("BOT_TOKEN")
+CHANNEL_ID: int = int(os.getenv("CHANNEL_ID"))
+HOURS: int = int(os.getenv("HOURS", 12))
+
+
+def get_clasifications():
+    with open("types.json", "r") as f:
+        return json.load(f)
+
+
+types = get_clasifications()
