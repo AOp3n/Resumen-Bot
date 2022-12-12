0c4decf6c399e04e06e3cdf01b9edf8648cc2ce3
Qwerty59a <116958040+Qwerty59a@users.noreply.github.com>
2022-12-12 18:24:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ from pyrogram import Client, filters, enums, idle
 
 bot = Client("my_account", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+@bot.on_edited_message(filters.chat(CHANNEL_ID) & filters.incoming & filters.regex(".(\(\S{1,2}\)).+"))
+async def add_to_resume(_, message):
+    resume_message = (await bot.get_chat(CHANNEL_ID)).pinned_message
+
+    sections = resume_message.text.markdown.split("\n❌")
+
+    title = message.matches[0].group(0)
+
+    substring_to_search = message.matches[0].group(1)
+    types_acronym = list(types.keys())
+
+    position = (types_acronym.index(substring_to_search) + 1) if substring_to_search in types_acronym else -1
+
+    extra = "\n" if position == -1 else ""
+
+    sections[position] += f"{extra}[{title}]({message.link})\n"
+
+    await resume_message.edit_text(text="\n❌".join(sections), parse_mode=enums.ParseMode.MARKDOWN,
+                                   disable_web_page_preview=True)
+
 
 @bot.on_message(filters.chat(CHANNEL_ID) & filters.incoming & filters.regex(".(\(\S{1,2}\)).+"))
 async def add_to_resume(_, message):
